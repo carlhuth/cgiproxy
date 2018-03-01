caac73cfddc2bc76a9f3dbddec7291554e8fab66
yakeworld <gushiedu@gmail.com>
2018-03-01 12:34:27 +0800

    Create readme.pl

---
+++ readme.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl  
+  
+use strict;  
+use warnings;  
+  
+print <<END  
+Content-type: text/html\n\n  
+<html>  
+    <body>  
+        <div>  
+            <a>shit!CGI...</a>  
+        </div>  
+    </body>  
+</html>  
+END 
